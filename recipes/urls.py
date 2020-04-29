40e68e1a2cf9fab032943371db99fbea42d4356f
slukas111 <sashamati@gmail.com>
2020-04-29 14:35:26 -0700

    established html <hello>, fixed url and views error

--- recipes/urls.py
+++ recipes/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from book.urls import urlpatterns as book_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += book_urls
