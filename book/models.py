74e31ed18b7bf7a13e6bc31ebe8b0b0c8cff6bb8
slukas111 <sashamati@gmail.com>
2020-05-01 07:45:44 -0700

    created model recipe/author. migrated to database. Created a super user.

--- book/models.py
+++ book/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+""" 
+Author model:
+
+Name (CharField)
+Bio (TextField)
+Recipe Model:
+
+Title (CharField)
+Author (ForeignKey)
+Description (TextField)
+Time Required (Charfield) (for example, "One hour")
+Instructions (TextField)
+
+"""
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+
+class RecipeItem(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField()
+    time_required = models.CharField(max_length=15)
+    instructions = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
