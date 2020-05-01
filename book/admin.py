f8e0857921435684d596594ee4ccc8cae8cf4325
slukas111 <sashamati@gmail.com>
2020-05-01 09:10:19 -0700

    function to create authors more readable. creating Authors admin side

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from book.models import Author, RecipeItem
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(RecipeItem)
--- book/models.py
+++ book/models.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 # Create your models here.
 """ 
 Author model:
-
 Name (CharField)
 Bio (TextField)
 Recipe Model:
@@ -22,6 +21,9 @@ class Author(models.Model):
     name = models.CharField(max_length=50)
     bio = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class RecipeItem(models.Model):
     title = models.CharField(max_length=50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -29,3 +31,6 @@ class RecipeItem(models.Model):
     time_required = models.CharField(max_length=15)
     instructions = models.TextField()
     date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
